e71842442d75bd83b42927385a739c6291e63bc5
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-07-11 14:39:23 +0530

    Added solution - LeetHub

---
+++ 700219--GFG/700219-.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    bool dfs(vector<vector<int>>& adj, int u, vector<bool>& vis, int parent) {
+        vis[u] = true;
+
+        for (int v : adj[u]) {
+            if (v == parent) continue;
+
+            if (vis[v]) return true;
+
+            if (dfs(adj, v, vis, u)) return true;
+        }
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>>& edges) {
+    vector<vector<int>> adj(V);
+    for(auto edge : edges){
+        int a = edge[0];
+        int b = edge[1];
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+        vector<bool> vis(V, false);
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (dfs(adj, i, vis, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
