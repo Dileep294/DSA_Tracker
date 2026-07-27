47f6c097868a01c8d5b1fc50e43841d28c33429a
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-07-27 19:26:57 +0530

    Added solution - Time: O(V * E), Space: O(V)

---
+++ 706218-bellman-ford-GFG/706218-bellman-ford.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> dist(V,1e8);
+        dist[src] = 0;
+        for(int i=0; i<V-1; i++){
+            for(auto &edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(dist[u]!=1e8 && dist[u]+w < dist[v]){
+                    dist[v] = dist[u]+w;
+                }
+            }
+            
+        }
+        for(auto &edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(dist[u]!=1e8 && dist[u]+w < dist[v]){
+                    return {-1};
+                }
+        }
+        return dist;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
