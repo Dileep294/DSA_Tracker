4bdb628ed39fabab3f3449b386846b492a45d824
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-04 15:45:55 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.76%) - LeetHub - Time: O(n), Space: O(1)

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
     int n = nums.size();
-    int sum =0;
+    int sum = nums[0];
     for(int i=0; i<n; i++){
         if(i>sum) return false;
         sum = max(sum, i+nums[i]);
