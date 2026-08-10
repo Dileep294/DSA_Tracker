f2abcda8c8ee10ed6b18ab40b952930474cdb58f
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-10 21:42:20 +0530

    Added solution - Time: O(n log k), Space: O(n)

---
+++ 705476-kth-largest-in-a-stream-GFG/705476-kth-largest-in-a-stream.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> kthLargest(vector<int>& arr, int k) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    vector<int> ans;
+    for(int i=0; i<arr.size(); i++){
+        pq.push(arr[i]);
+        
+        if(pq.size()>k) pq.pop();
+        
+        if(pq.size()<k) ans.push_back(-1);
+        else ans.push_back(pq.top());
+    }
+    return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
