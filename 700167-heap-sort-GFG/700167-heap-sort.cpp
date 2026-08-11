d6f74e239bf8234212b24d5bfc397460df2cf2c0
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-11 21:11:24 +0530

    Added solution - Time: O(N log N), Space: O(log N)

---
+++ 700167-heap-sort-GFG/700167-heap-sort.cpp
@@ -0,0 +1,14 @@
+// The functions should be written in a way that array become sorted
+// in increasing order when heapSort() is called.
+
+class Solution {
+  public:
+    // Function to sort an array using Heap Sort.
+    void heapSort(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
