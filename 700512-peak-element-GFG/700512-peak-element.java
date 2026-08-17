6f639c4b1af6df0323829d0a9a7e6cbae3532827
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-17 12:32:56 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 700512-peak-element-GFG/700512-peak-element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int peakElement(int[] arr) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
