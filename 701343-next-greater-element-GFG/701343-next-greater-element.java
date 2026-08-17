a3561d57473b0f32ea11c99725f8de3c80b8097e
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-17 15:24:18 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701343-next-greater-element-GFG/701343-next-greater-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+    int n = arr.length;
+    Stack<Integer> st = new Stack<>();
+    ArrayList<Integer> ans = new ArrayList<>();
+    for(int i=n-1; i>=0; i--){
+        while(!st.isEmpty() && st.peek() <= arr[i]){
+            st.pop();
+        }
+        if(st.isEmpty()){
+            ans.add(-1);
+        }
+        else{
+            ans.add(st.peek());
+        }
+        st.push(arr[i]);
+    }
+    Collections.reverse(ans);
+    return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
