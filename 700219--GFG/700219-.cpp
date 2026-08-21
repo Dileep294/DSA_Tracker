a7207537c72ce14d409c32704b586914cc7f44cf
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-21 18:35:27 +0530

    Added solution

--- 700219--GFG/700219-.cpp
+++ 700219--GFG/700219-.cpp
@@ -1,36 +1,30 @@
 class Solution {
   public:
-    bool dfs(vector<vector<int>>& adj, int u, vector<bool>& vis, int parent) {
-        vis[u] = true;
-
-        for (int v : adj[u]) {
-            if (v == parent) continue;
-
-            if (vis[v]) return true;
-
-            if (dfs(adj, v, vis, u)) return true;
+    bool isCyclebfs(vector<vector<int>>& adj,int u,vector<bool>& visited,int parent){ 
+        visited[u] = true;
+        for(int &v : adj[u]){
+            if(v==parent) continue;
+            if(visited[v]) return true;
+            if(isCyclebfs(adj,v,visited,u)){
+                return true; 
+            } 
         }
         return false;
-    }
-
+        }
     bool isCycle(int V, vector<vector<int>>& edges) {
     vector<vector<int>> adj(V);
     for(auto edge : edges){
         int a = edge[0];
         int b = edge[1];
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-        vector<bool> vis(V, false);
-
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                if (dfs(adj, i, vis, -1)) {
-                    return true;
-                }
-            }
+        adj[a].push_back(b); adj[b].push_back(a);
+        } 
+        vector<bool> visited(V,false);
+        for(int i=0; i<V; i++){
+            if(!visited[i] && isCyclebfs(adj,i,visited,-1)){
+                return true;
+            } 
         }
-        return false;
+     return false;    
     }
 };
 
