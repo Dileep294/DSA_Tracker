e8cac2278923e65532361506226828ac4a51b0c3
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-23 18:15:05 +0530

    Added solution

---
+++ 702807-string-duplicates-removal-GFG/702807-string-duplicates-removal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string removeDuplicates(string &s) {
+    string ans="";
+    for(int i=0; i<s.length(); i++){
+        if(ans.find(s[i])==string::npos){
+            ans +=s[i];
+        }
+    }
+    return ans;    
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
