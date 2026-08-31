5909b3998cb5497ae08327e8123cde49d9d76b84
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-08-31 20:31:17 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 911734-string-stack-GFG/911734-string-stack.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool stringStack(string &pat, string &tar) {
+        int i = pat.size() - 1;
+        int j = tar.size() - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (pat[i] != tar[j]) {
+                // Current character is used for deletion
+                // and deletes the previous character
+                i -= 2;
+            }
+            else {
+                // Current character remains in final string
+                i--;
+                j--;
+            }
+        }
+
+        return j < 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
