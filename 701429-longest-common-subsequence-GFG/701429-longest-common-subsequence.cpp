394d93580405003ee5c8d13a7c88a2f040cf8220
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-09-03 21:50:32 +0530

    Added solution - Time: O(n*m), Space: O(n*m)

--- 701429-longest-common-subsequence-GFG/701429-longest-common-subsequence.cpp
+++ 701429-longest-common-subsequence-GFG/701429-longest-common-subsequence.cpp
@@ -1,24 +1,20 @@
 class Solution {
   public:
-    vector<vector<int>> dp;
-    int help(string &s1, string &s2,int i,int j){
-        if(i==s1.size() || j==s2.size()) return 0;
-        //int count = 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]){
-          // count = count+1;
-          return dp[i][j] = 1 + help(s1,s2,i+1,j+1);
-        }
-        else {
-           return dp[i][j] = max(help(s1,s2,i+1,j),help(s1,s2,i,j+1));
-        }
-        
-    }
     int lcs(string &s1, string &s2) {
     int n = s1.size();
     int m = s2.size();
-    dp.assign(n+1,vector<int>(m+1,-1));
-    return help(s1,s2,0,0);
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
     }
 };
 
