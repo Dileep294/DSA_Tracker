83e7fc43d10e9235ac363f9d8eb1cc9774b0a393
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-09-12 18:09:35 +0530

    Added solution - Time: O(√n + k log k), Space: O(k)

---
+++ 712172-all-divisors-of-a-number-GFG/712172-all-divisors-of-a-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<Integer> getDivisors(int n) {
+    ArrayList<Integer> arr = new ArrayList<>();
+    for(int i=1; i*i<=n; i++){
+        if(n%i==0){
+            arr.add(i);
+            
+            if(i != n/i){
+                arr.add(n/i);
+            }
+        }
+    }
+   // Collections.sort(sort);
+    Collections.sort(arr);
+    return arr;    
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
