cbfa0ab0ef3fefa895fdbb9d6965871805d0d4bb
Dileep Kumar Prajapati <143834343+Dileep294@users.noreply.github.com>
2026-09-25 18:30:55 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703505-count-words-in-string-GFG/703505-count-words-in-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countWords(String s) {
+        // code here
+    String[] word = s.trim().split("\\s+");
+    return word.length;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
